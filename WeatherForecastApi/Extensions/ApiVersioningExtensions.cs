d216bfc3c68fb758193187c4597d9b979329d08f
Juanlu <juan.luis@nextchapter-ecommerce.com>
2022-11-30 22:18:39 +0100

    Remove unnecesary using

--- WeatherForecastApi/Extensions/ApiVersioningExtensions.cs
+++ WeatherForecastApi/Extensions/ApiVersioningExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using WeatherForecastApi.Filters;
