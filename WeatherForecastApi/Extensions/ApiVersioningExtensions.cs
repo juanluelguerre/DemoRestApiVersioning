8ee19246c08ff5e6e5d1ee48b6ec09601012d570
Juanlu <juan.luis@nextchapter-ecommerce.com>
2022-11-30 22:17:52 +0100

    IncludeXmlComments for more than one assembly !

--- WeatherForecastApi/Extensions/ApiVersioningExtensions.cs
+++ WeatherForecastApi/Extensions/ApiVersioningExtensions.cs
@@ -10,12 +10,12 @@ public static class ApiVersioningExtensions
 {
     public static IServiceCollection AddCustomApiVersioning(this IServiceCollection services)
     {
-        services.AddApiVersioning(config =>
+        services.AddApiVersioning(options =>
         {
-            config.AssumeDefaultVersionWhenUnspecified = true;
-            config.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
             // Allows API return versions in the response header (api-supported-versions).
-            config.ReportApiVersions = true;
+            options.ReportApiVersions = true;
             // Allows to choose whether they would like to place the parameter in the URL or in the request header
             //config.ApiVersionReader = ApiVersionReader.Combine(
             //    new UrlSegmentApiVersionReader(),
@@ -24,17 +24,34 @@ public static class ApiVersioningExtensions
         });
 
         // Allows to discover versions
-        services.AddVersionedApiExplorer(config =>
+        services.AddVersionedApiExplorer(options =>
         {
-            config.GroupNameFormat = "'v'VVV";
-            config.SubstituteApiVersionInUrl = true;
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
         });
 
-        services.AddSwaggerGen(config =>
+        services.AddSwaggerGen(options =>
         {
-            config.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
-                $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"), true);
-            config.OperationFilter<SwaggerDefaultValuesFilter>();
+            // TODO: Use this sentence if you use only one assembly with comments.
+            // options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+            //    $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"), true);
+
+            // TODO: Replace according to your namespace. ej: "ElGuerre.Versioning.xxx."
+            //       Use this one if you've comments in more than one assembly.
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(x => x.GetName().Name?.StartsWith("WeatherForecastApi") ?? false);
+
+            foreach (var assembly in assemblies)
+            {
+                var xmlPath = Path.Combine(AppContext.BaseDirectory,
+                    $"{assembly.GetName().Name}.xml");
+                if (File.Exists(xmlPath))
+                {
+                    options.IncludeXmlComments(xmlPath);
+                }
+            }
+
+            options.OperationFilter<SwaggerDefaultValuesFilter>();
         });
 
         services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
